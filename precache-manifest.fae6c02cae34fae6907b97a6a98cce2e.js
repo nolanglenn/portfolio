1d59cf9fcbc481a5e4ddf7dd3621a05a22b79155
nolanglenn <nolangglenn@gmail.com>
2019-06-10 18:51:28 -0500

    Updates

---
+++ precache-manifest.fae6c02cae34fae6907b97a6a98cce2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7c3ac8633ac47ae0e091179b2ad75b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85c74ca9e0782bc5890b",
+    "url": "/portfolio/static/css/main.76a789f4.chunk.css"
+  },
+  {
+    "revision": "f35135349b7e039b7092",
+    "url": "/portfolio/static/js/2.100c8e3f.chunk.js"
+  },
+  {
+    "revision": "85c74ca9e0782bc5890b",
+    "url": "/portfolio/static/js/main.1fbd16fb.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "bf1277d455d1ed09bb0c303ffc59268e",
+    "url": "/portfolio/static/media/profilepic.bf1277d4.jpg"
+  },
+  {
+    "revision": "b9f5092152a225e8a79b00b341d5c181",
+    "url": "/portfolio/static/media/tasktrader.b9f50921.PNG"
+  }
+]);
